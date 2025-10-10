76850d233a281d7103a869c5725583d34f5d6685
julialg2002 <s2107370@ed.ac.uk>
2025-10-10 12:23:27 +0100

    fixed pendulum dynamics eq

--- implementation/src/pendulum_ml/dynamics/pendulum.py
+++ implementation/src/pendulum_ml/dynamics/pendulum.py
@@ -12,10 +12,13 @@ def f(x: np.ndarray, u: float, p: Params) -> np.ndarray:
     Returns:
         np.ndarray: state derivative [theta_dot, theta_ddot]
     """
-    #TODO: figure out why there's a +0.0*theta_ddot in the return statement and how to formulate dynamics correctly
     
-    theta, theta_dot = float(x[0]), float(x[1])
+    theta = float(x[0])
+    theta_dot = float(x[1])
     
-    theta_ddot = (p.g / p.L) * np.sin(theta) + (1.0 / (p.m * p.L**2)) * (u - p.c * theta_dot)
-    
-    return np.array([theta + 0.0*theta_dot, theta_ddot], dtype=float)  # return [θ̇, θ̈]
+    # Dynamics equations
+    theta_ddot = - (p.g / p.l) * np.sin(theta) \
+                 + (1.0 / p.m * p.l**2) * float(u) \
+                 - (p.c / (p.m * p.l**2)) * theta_dot
+                 
+    return np.array([theta_dot, theta_ddot], dtype=float)

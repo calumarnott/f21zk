3f05d608bcfe6881fb689734289f2f158379d02b
julialg2002 <s2107370@ed.ac.uk>
2025-10-14 17:34:43 +0100

    nn animation working

--- implementation/src/pendulum_ml/animate.py
+++ implementation/src/pendulum_ml/animate.py
@@ -1,18 +1,15 @@
 import numpy as np
 import torch
-from .dynamics.base import rk4_step, wrap_to_pi
+from .dynamics.base import rk4_step, wrap_to_pi, control_error, build_controllers_from_cfg
 
-def generate_trajectory_from_NN_controller(cfg, cps, model):
+def generate_trajectory_from_NN_controller(cfg, cps, model, initial_states):
     """ Generate a trajectory using a learned NN controller.
 
     Args:
         cfg (dict): Configuration dictionary containing simulation parameters.
         cps (module): dynamics module (e.g. pendulum_ml.dynamics.pendulum)
         model (torch.nn.Module): trained neural network controller
-        x0 (np.ndarray): initial state
-        T (float): total simulation time
-        dt (float): simulation time step
-        dt_ctrl (float): control time step
+        initial_states (list or np.ndarray): list or array of initial states, shape (num_trajectories, state_dim)
 
     Returns:
         np.ndarray: trajectory array of shape (num_steps, state_dim + num_control_axes)
@@ -20,9 +17,9 @@ def generate_trajectory_from_NN_controller(cfg, cps, model):
     model.eval()  # set model to evaluation mode
     dt = float(cfg["dynamics"].get("dt", 0.01))  # simulation time step
     dt_ctrl = float(cfg["dynamics"].get("control_dt", dt)) # controller
-    x0 = np.array(cfg["data"]["initial_state"])
+    init_states = np.array(initial_states) # list of initial states
     T = float(cfg["data"]["sim_time"])
-    assert x0.shape == (len(cps.STATE_NAMES),), f"Initial state shape mismatch. Expected {(len(cps.STATE_NAMES),)}, got {x0.shape}"
+    assert init_states.shape[1] == len(cps.STATE_NAMES), f"Initial state shape mismatch. Expected {len(cps.STATE_NAMES)}, got {x0.shape[1]}"
     
     num_steps = int(T / dt)
     n_ctrl_steps = int(dt_ctrl / dt)
@@ -31,26 +28,65 @@ def generate_trajectory_from_NN_controller(cfg, cps, model):
     params = cps.validate_params(cps.Params, cfg["dynamics"]["params"])
     
     
-    trajectory = np.zeros((num_steps, state_dim + num_controls), dtype=float)
-    x = x0.copy()
+    trajectory = np.zeros((num_steps * len(init_states), 1 + state_dim + num_controls), dtype=float)
+
     t = 0.0
     control_steps_counter = 0
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+    elif torch.backends.mps.is_available():
+        device = torch.device("mps")
+    else:
+        device = torch.device("cpu")
+    model.to(device)
+    
+    controllers = build_controllers_from_cfg(cfg["controller"], cps.CONTROL_AXES)
+    for c in controllers.values():
+        c.reset()
     
     with torch.no_grad():
-        for i in range(num_steps):
-            if control_steps_counter % n_ctrl_steps == 0:
-                # Time to compute new control inputs
-                x_tensor = torch.tensor(x, dtype=torch.float32).unsqueeze(0)  # shape (1, state_dim)
-                u_tensor = model(x_tensor)  # shape (1, num_controls)
-                u = u_tensor.squeeze(0).numpy()  # shape (num_controls,)
-            control_steps_counter += 1
+        for k in range(len(init_states)): # for each init state
+            x0 = init_states[k]
+            x = x0.copy()
             
-            # Step the dynamics with current control inputs
-            x = rk4_step(x, {axis: float(u[j]) for j, axis in enumerate(cps.CONTROL_AXES)}, cps.f, params, dt)
-            x[0] = wrap_to_pi(x[0])  # wrap angle theta to [-pi, pi]
-            t += dt
+            for c in controllers.values():
+                c.reset()
+                
+            err_dict = {}
             
-            trajectory[i, :state_dim] = x
-            trajectory[i, state_dim:] = u
+            for axis, ctrl in controllers.items():
+                sp = cfg["controller"]["pid"].get(axis, {}).get("setpoint", 0.0)
+                err_dict[axis] = control_error(cps, axis, x, sp)
+
+            x_input = np.concatenate([x, [err_dict[axis] for axis in cps.CONTROL_AXES]], axis=0)
+            t = 0.0
+            x_traj = np.concatenate([[t], x_input], axis=0)
             
+            trajectory[k*num_steps] = x_traj
+
+            for i in range(1, num_steps):
+                if control_steps_counter % n_ctrl_steps == 0:
+                    # Time to compute new control inputs
+                    x_tensor = torch.tensor(x_input, dtype=torch.float32).unsqueeze(0)  # shape (1, state_dim + num_control_axes)
+                    # to device
+                    x_tensor = x_tensor.to(device)
+                    u_tensor = model(x_tensor)  # shape (1, num_controls)
+                    u = u_tensor.squeeze(0).cpu().numpy()  # shape (num_controls,)
+                control_steps_counter += 1
+                
+                # Step the dynamics with current control inputs
+                x = rk4_step(x, {axis: float(u[j]) for j, axis in enumerate(cps.CONTROL_AXES)}, cps.f, params, dt)
+                x[0] = wrap_to_pi(x[0])  # wrap angle theta to [-pi, pi]
+                t += dt
+                
+                for axis, ctrl in controllers.items():
+                    sp = cfg["controller"]["pid"].get(axis, {}).get("setpoint", 0.0)
+                    err = control_error(cps, axis, x, sp)
+                    err_dict[axis] = float(err)
+                    
+                x_input = np.concatenate([x, [err_dict[axis] for axis in cps.CONTROL_AXES]], axis=0)
+                x_traj = np.concatenate([[t], x_input], axis=0)   
+
+                trajectory[k*num_steps + i] = x_traj  # state + error
+
     return trajectory
